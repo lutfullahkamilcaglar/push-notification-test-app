4f371d9c314d93fb3cb63fb6de2eb622919dbd2f
kamilcaglar <alarkamil34@gmail.com>
2023-07-19 20:07:08 +0200

    firebase added push notification added

--- PushNotification/AppDelegate.swift
+++ PushNotification/AppDelegate.swift
@@ -5,24 +5,47 @@
 //  Created by Kamil Caglar on 19/07/2023.
 //
 
+import Firebase
+import FirebaseMessaging
+import UserNotificationsUI
 import UIKit
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate, UNUserNotificationCenterDelegate {
 
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]?) -> Bool {
         
+        FirebaseApp.configure()
+        Messaging.messaging().delegate = self
+        UNUserNotificationCenter.current().delegate = self
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { succes, _ in
+            guard succes else {
+                return
+            }
+    print("Succes in APNS registery")
+}
+        
+        application.registerForRemoteNotifications()
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
         
         window?.rootViewController = NotificationViewController()
         
-        
         return true
         
+    }
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        messaging.token { token, _ in
+            guard let token = token else {
+                return
+            }
+            print("Token: \(token)")
+        }
     }
 
 }
